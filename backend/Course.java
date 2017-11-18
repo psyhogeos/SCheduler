f62d71406e8912b59ff344c90ad4ead940a0d2dc
Thomas Psyhogeos <psyhogeo@usc.edu>
2017-11-18 14:46:26 -0800

    Edit Course.java

--- backend/Course.java
+++ backend/Course.java
@@ -2,10 +2,13 @@ import java.util.ArrayList;
 
 public class Course {
 	private ArrayList<Course> prereqs = new ArrayList<Course>();
-	private ArrayList<Course> dependencies = new ArrayList<Course>();
-	private String name;
-	public Course(String name) {
-		this.name = name;
+	private String prefix;
+	private String num;
+	private int units;
+	public Course(String prefix, String num, int units) {
+		this.prefix = prefix;
+		this.num = num;
+		this.units = units;
 	}
 	public ArrayList<Course> getPrereqs() {
 		return prereqs;
@@ -13,19 +16,25 @@ public class Course {
 	public void setPreqs(ArrayList<Course> prereqs) {
 		this.prereqs = prereqs;
 	}
-	public ArrayList<Course> getDependencies() {
-		return dependencies;
+	public String getPrefix() {
+		return prefix;
 	}
-	public void setDependencies(ArrayList<Course> dependencies) {
-		this.dependencies = dependencies;
+	public void setPrefix(String prefix) {
+		this.prefix = prefix;
 	}
-	public String getName() {
-		return name;
+	public String getNum() {
+		return num;
 	}
-	public void setName(String name) {
-		this.name = name;
+	public void setNum(String num) {
+		this.num = num;
 	}
 	public void setPrereqs(ArrayList<Course> prereqs) {
 		this.prereqs = prereqs;
+	}
+	public int getUnits() {
+		return units;
+	}
+	public void setUnits(int units) {
+		this.units = units;
 	}
 }
